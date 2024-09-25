9bfef6f7d7d95faed14d6611d2180ac70e057ae7
aisa <aisabihorac965@gmail.com>
2024-09-25 20:24:31 +0200

    Lekcija17

---
+++ topRatedmovies.js
@@ -0,0 +1,12 @@
+
+const  topRatedURL=`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${apiKey}`
+fetch(topRatedURL,options).then(response=>response.json()).then(data=>{
+    console.log(data);
+    const topRatedMovies=data.results;
+    topRatedMovies.forEach(item=>{
+        const filmPosterURL=imageURL+item.poster_path;
+        const itemHTML=`<img src="${filmPosterURL}" height="150px" class="m-1">`;
+        document.getElementById("movies-container").innerHTML+=itemHTML;
+    });
+});
+   
