1373f5917335d79ba4ee4f76e009254db7878d50
ssorguc <sorgucselma@gmail.com>
2024-10-06 12:36:42 +0300

    image url not defined FIX

--- topRatedmovies.js
+++ topRatedmovies.js
@@ -1,12 +1,13 @@
+'use strict';
 
-const  topRatedURL=`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${apiKey}`
-fetch(topRatedURL,options).then(response=>response.json()).then(data=>{
+const imageURL = "https://image.tmdb.org/t/p/original/";
+const topRatedURL = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${apiKey}`
+fetch(topRatedURL, options).then(response => response.json()).then(data => {
     console.log(data);
-    const topRatedMovies=data.results;
-    topRatedMovies.forEach(item=>{
-        const filmPosterURL=imageURL+item.poster_path;
-        const itemHTML=`<img src="${filmPosterURL}" height="150px" class="m-1">`;
-        document.getElementById("movies-container").innerHTML+=itemHTML;
+    const topRatedMovies = data.results;
+    topRatedMovies.forEach(item => {
+        const filmPosterURL = imageURL + item.poster_path;
+        const itemHTML = `<img src="${filmPosterURL}" height="250px" class="m-1">`;
+        document.getElementById("movies-container").innerHTML += itemHTML;
     });
 });
-   
