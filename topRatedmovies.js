6f3eba81cd354d6684b5c567702180983e23c921
ssorguc <sorgucselma@gmail.com>
2024-10-13 14:21:49 +0300

    Removing api token and generating new one

--- topRatedmovies.js
+++ topRatedmovies.js
@@ -1,16 +1,8 @@
 'use strict';
 
-const apiKey = "80b072d8f2331c1f770690aee2833931";
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: "Bearer yJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MGIwNzJkOGYyMzMxYzFmNzcwNjkwYWVlMjgzMzkzMSIsIm5iZiI6MTcyNjk5ODY5My45ODY5NzcsInN1YiI6IjY2ZTMyY2YxZjQ2N2MyYWQ2MmY5MjFhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wt0ljvMDua5hgNAgqzCCTT793ycjX6wlaDz5chDXC2E"
-  }
-};
-let page=0;
+let page = 0;
 const imageURL = "https://image.tmdb.org/t/p/original/";
- let topRatedURL = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}1&api_key=${apiKey}`
+let topRatedURL = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}1&api_key=${apiKey}`
 
 fetch(topRatedURL, options).then(response => response.json()).then(data => {
     page++;
@@ -19,10 +11,10 @@ fetch(topRatedURL, options).then(response => response.json()).then(data => {
         const filmPosterURL = imageURL + item.poster_path;
         const itemHTML = `<img src="${filmPosterURL}" height="250px" class="m-1">`;
         document.getElementById("movies-container").innerHTML += itemHTML;
-     
+
     });
 });
-function loadMore(){
+function loadMore() {
     topRatedURL = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}1&api_key=${apiKey}`
     fetch(topRatedURL, options).then(response => response.json()).then(data => {
         page++;
